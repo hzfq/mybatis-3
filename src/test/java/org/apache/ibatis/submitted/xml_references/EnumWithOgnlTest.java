e8acc0ea432991a81e576f7c1255419da40fb8bf
huzhifengqing <1443754157@qq.com>
2019-04-21 00:18:56 +0800

    格式化代码

--- src/test/java/org/apache/ibatis/submitted/xml_references/EnumWithOgnlTest.java
+++ src/test/java/org/apache/ibatis/submitted/xml_references/EnumWithOgnlTest.java
@@ -1,22 +1,19 @@
 /**
- *    Copyright 2009-2019 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2019 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.submitted.xml_references;
-
-import java.io.Reader;
-import java.util.Properties;
 
 import org.apache.ibatis.datasource.unpooled.UnpooledDataSourceFactory;
 import org.apache.ibatis.io.Resources;
@@ -27,6 +24,9 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.apache.ibatis.session.defaults.DefaultSqlSessionFactory;
 import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
 import org.junit.jupiter.api.Test;
+
+import java.io.Reader;
+import java.util.Properties;
 
 class EnumWithOgnlTest {
 
@@ -46,11 +46,12 @@ class EnumWithOgnlTest {
         configuration.addMapper(PersonMapper2.class);
         new DefaultSqlSessionFactory(configuration);
     }
+
     @Test
     void testMixedConfiguration() throws Exception {
-      try (Reader reader = Resources.getResourceAsReader("org/apache/ibatis/submitted/xml_references/ibatisConfig.xml")) {
-          SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
-          sqlSessionFactory.getConfiguration().addMapper(PersonMapper2.class);
-      }
+        try (Reader reader = Resources.getResourceAsReader("org/apache/ibatis/submitted/xml_references/ibatisConfig.xml")) {
+            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+            sqlSessionFactory.getConfiguration().addMapper(PersonMapper2.class);
+        }
     }
 }
